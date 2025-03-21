100ec4a524ac7e72ae7749b2fd44a05eb75ac019
Manou00225 <paulkama225@gmail.com>
2025-03-21 08:59:28 +0000

    Mise a jour du code Shell pour Footprinting

--- shell.sh
+++ shell.sh
@@ -1,71 +1,56 @@
-echo " ***************** FOOTPRINTING TOOLS **********************" 
-echo " *                                                         *"
-echo " *                                                         *" 
-echo " * 1    FOOTPRINTING WITH USING WHOIS                      *"
-echo " * 2    FOOTPRINTING WITH USING DIG                        *"
-echo " * 3    FOOTPRINTING WITH USING THE HARVESTER              *"
-echo " * 4    FOOTPRINTING WITH USING TRACE ROUTE                *"
-echo " * 5    FOOTPRINTING WITH USING HOST                       *"
-echo " * 6    FOOTPRINTING WITH USING NSLOOKUP                   *"
-echo " *                                                         *"
-echo " ***********************************************************"
+#!/bin/bash
 
-echo " Veuillez faire une choix de la plateforme a utiliser : " 
-read choice 
+while true; do
+    echo " ***************** FOOTPRINTING TOOLS **********************"
+    echo " *                                                         *"
+    echo " * 1    FOOTPRINTING WITH USING WHOIS                      *"
+    echo " * 2    FOOTPRINTING WITH USING DIG                        *"
+    echo " * 3    FOOTPRINTING WITH USING THE HARVESTER              *"
+    echo " * 4    FOOTPRINTING WITH USING TRACE ROUTE                *"
+    echo " * 5    FOOTPRINTING WITH USING HOST                       *"
+    echo " * 6    FOOTPRINTING WITH USING NSLOOKUP                   *"
+    echo " *                                                         *"
+    echo " ***********************************************************"
+    domaine=$1
+    choice=$2
 
-echo " "
-echo " Veuillez entrer un domaine a chercher : " 
-read domaine
+    echo " $1"
+    echo " $2"
 
-dir=Footprinting-$domaine
-mkdir $dir
-cd $dir
+    dir=footprinting-$domaine
+    mkdir -p $dir
+    cd $dir
 
-if [ $choice = 1 ]; then
-	echo " ************************ FOOTPRINTING WITH WHOIS TOOL ********* "
-	whois $domaine > whois.txt
+    if [ "$choice" = "1" ]; then
+        echo " ************************ FOOTPRINTING WITH WHOIS TOOL ********* "
+        whois "$domaine" > whois.txt
+    elif [ "$choice" = "2" ]; then
+        echo " *********************** FOOTPRINTING WITH DIG *****************"
+        dig "$domaine" > dig.txt
+    elif [ "$choice" = "3" ]; then
+        echo " ********************* FOOTPRINTING WITH THE HARVESTER ******** "
+        theHarvester -d "$domaine" -l 200 -b yahoo
+    elif [ "$choice" = "4" ]; then
+        echo " ******************* FOOTPRINTING WITH TRACE ROUTE *************"
+        traceroute "$domaine"
+    elif [ "$choice" = "5" ]; then
+        echo " ***************** FOOTPRINTING WITH HOST *******************"
+        host "$domaine"
+    elif [ "$choice" = "6" ]; then
+        echo " ***************** FOOTPRINTING WITH NSLOOKUP ****************"
+        nslookup "$domaine"
+    else
+        echo " Choix invalide ! Veuillez sélectionner un numéro entre 1 et 6."
+    fi
 
-fi
- 
-if [ $choice = 2 ] ; then
-	echo " *********************** FOOTPRINTING WITH DIG *****************"
- 	dig $domaine > dig.txt
-
-fi
-
-if [ $choice = 3 ] ; then
-	echo " ********************* FOOTPRINTING WITH THE HARVESTER ******** "
-	theHarvester -d $domaine -l 200 -b yahoo > theHarvester.txt
-
-fi
-
-
-if [ $choice = 4 ] ; then 
-	echo " ******************* FOOTPRINTING WITH TRACE ROUTE *************"
- 	traceroute $domaine > traceroute.txt 
-
-fi
-
-if [ $choice = 5 ] ; then
-	echo " ***************** FOOTPRINTING WITH HOST *******************"
-	host $domaine > host.txt
-
-fi
-
-
-if [ $choice = 6 ] ; then 
-	echo " ***************** FOOTPRINTING WITH NSLOOKUP ****************"
-	nslookup $domaine > nslookup.txt
-
-fi
-
-echo " "
-
-echo " Voulez-vous faire une autre session ? (o/n) : "
+    echo " "
+    echo " Voulez-vous faire une autre session ? (o/n) : "
     read replay
     if [ "$replay" != "o" ]; then
         echo " Merci d'avoir utilisé notre logiciel (*_*) "
+        break
     fi
+done
 
 exit
 
